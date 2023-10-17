c328d5fd7ffde960c0cfc5b821254d54712f9543
humberto <humberto.gnm98@gmail.com>
2023-10-17 11:33:22 -0300

    Select and show specific email

--- src/Components/Footer/InboxDetails.tsx
+++ src/Components/Footer/InboxDetails.tsx
@@ -1,19 +1,29 @@
-export default function InboxDetails() {
+import { IMail } from '../../Hooks/useGetEmails'
+
+interface IProps {
+  email: IMail | undefined,
+  notEmpty: boolean,
+}
+
+export default function InboxDetails({ email, notEmpty }: IProps) {
   return (
     <div
       className="flex flex-col p-1 border-t-1 border-zinc-300 dark:border-zinc-700 mt-12 col-span-2"
     >
-      <h3 className="font-semibold p-2">Welcome</h3>
-      <div className="h-full rounded-md bg-white dark:bg-zinc-800 border-1.5 border-zinc-300 dark:border-zinc-700 p-3">
-        <p className="h-full">
-          Hi drf,
-          Your temp e-mail address is ready
-          If you need help read the information below and do not hesitate to contact us.
-
-          All the best,
-          DropMail
-        </p>
-      </div>
+      {notEmpty ?
+        (
+          <>
+            <div className="flex flex-row justify-between items-center">
+              <h3 className="font-semibold p-2">{email?.headerSubject}</h3>
+              <span className="text-sm font-light">from {email?.fromAddr}</span>
+            </div>
+            <div className="h-full rounded-md bg-white dark:bg-zinc-800 border-1.5 border-zinc-300 dark:border-zinc-700 p-3">
+              <p className="h-5/6">{email?.text}</p>
+              <a className="float-right text-sm" href={email?.downloadUrl}>Download email</a>
+            </div>
+          </>
+        ) : <p>No email selected</p>
+      }
     </div>
   )
 }
--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
 import InboxDetails from './InboxDetails'
 import InboxList from './InboxList'
+import useGetEmails, { IMail } from '../../Hooks/useGetEmails'
 
 export default function Footer() {
+  const { emails } = useGetEmails()
+  const [selectedEmail, setSelectedEmail] = useState<IMail>()
+
+  const handleSelectEmail = (id: number) => {
+    setSelectedEmail(emails[id])
+  }
+
   return (
     <footer className="border-t-1 border-zinc-300 dark:border-zinc-700 h-96 grid grid-cols-1 sm:grid-cols-3">
-      <InboxList />
-      <InboxDetails />
+      <InboxList emails={emails} handleSelectEmail={handleSelectEmail} />
+      <InboxDetails email={selectedEmail} notEmpty={!!selectedEmail} />
     </footer>
   )
 }
