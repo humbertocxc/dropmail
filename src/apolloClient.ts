7053c7ceca5432bb4bce8e1488a4e08b55c7c40f
humberto <humberto.gnm98@gmail.com>
2024-09-28 14:37:07 -0300

    remove cors-anywhere

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
-import { VITE_KEY, VITE_PROXY, VITE_URI } from './utils/env/env'
+import { VITE_KEY, VITE_URI } from './utils/env/env'
 
 const client = new ApolloClient({
-  uri: `${VITE_PROXY}/${VITE_URI}/${VITE_KEY}`,
+  uri: `${VITE_URI}/${VITE_KEY}`,
   cache: new InMemoryCache(),
 })
 
--- src/utils/env/env.ts
+++ src/utils/env/env.ts
@@ -1,3 +1,2 @@
-export const VITE_PROXY = import.meta.env.VITE_PROXY || 'https://cors-anywhere.herokuapp.com'
 export const VITE_URI = import.meta.env.VITE_URI || 'https://dropmail.me/api/graphql'
 export const VITE_KEY = import.meta.env.VITE_KEY || 'CustomTok3n'
